62efcb2fea62a2a3aadd93b24b10e8627a58fa07
Henry Nawrocki <hdev@sztanski.com>
2021-09-15 10:45:45 -0500

    first commit with register signup components and welcome view

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCq6PuwuZVuvrwMqyiGpcaox2wdGkYrgdA',
+  authDomain: 'vue-firebase-sites-peluso.firebaseapp.com',
+  projectId: 'vue-firebase-sites-peluso',
+  storageBucket: 'vue-firebase-sites-peluso.appspot.com',
+  messagingSenderId: '550483390363',
+  appId: '1:550483390363:web:322cf90a009c8b708dbb3e',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import './assets/main.css'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
